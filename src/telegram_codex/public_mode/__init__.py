a9ae1962b4b7c0bf9fff6a1e83f112fa0f35c8b6
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-08-23 21:13:50 +0700

    refactor: expose Public Mode application primitives

--- src/telegram_codex/public_mode/__init__.py
+++ src/telegram_codex/public_mode/__init__.py
@@ -4,6 +4,8 @@ This package deliberately does not use personal MTProto sessions. It models
 Telegram Business connections as delegated, revocable capabilities.
 """
 
+from .connections import ConnectionBinding, InMemoryBusinessConnectionRegistry
+from .linking import LinkTokenInvalid, OneTimeTelegramLinkBroker
 from .models import (
     BusinessConnection,
     BusinessMessageEvent,
@@ -13,13 +15,25 @@ from .models import (
     RetentionMode,
     RetentionPolicy,
 )
+from .policy import BusinessPermissionDenied
+from .service import PublicConnectionRequired, PublicTelegramService
+from .store import MemoryTTLBusinessEventStore, NullBusinessEventStore
 
 __all__ = [
     "BusinessConnection",
     "BusinessMessageEvent",
+    "BusinessPermissionDenied",
     "BusinessRights",
+    "ConnectionBinding",
     "EventKind",
+    "InMemoryBusinessConnectionRegistry",
+    "LinkTokenInvalid",
+    "MemoryTTLBusinessEventStore",
+    "NullBusinessEventStore",
+    "OneTimeTelegramLinkBroker",
     "PublicAction",
+    "PublicConnectionRequired",
+    "PublicTelegramService",
     "RetentionMode",
     "RetentionPolicy",
 ]
